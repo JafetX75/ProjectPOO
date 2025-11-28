3746698309ec35e7d70c29908678cfb3983408c8
JafetX75 <jafetxito75@gmail.com>
2025-11-27 22:01:28 -0600

    kit-tools

---
+++ src/resultado.java
@@ -0,0 +1,12 @@
+public class resultado {
+    private int x;
+    private int y;
+
+    public resultado(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int Suma1(){
+        return x + y;
+    }
+}
