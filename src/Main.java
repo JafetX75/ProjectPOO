d3dd3b99edc16048c327cda758aef69bfd01e208
JafetX75 <jafetxito75@gmail.com>
2025-12-09 12:58:22 -0600

    Trabajos unidad4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        ArregloBidimensional Alv = new ArregloBidimensional(10,10);
-
-
-        Alv.Cuadrado();
-        //Alv.Altura();
-        //Alv.anchuraNumero();
-        Alv.Renderizado();
    }
 }
